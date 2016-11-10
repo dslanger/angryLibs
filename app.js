2ffbb46a71da999b5704d5c2bc5452bca7627ca7
dslanger <dslanger@gmail.com>
2016-11-09 21:24:18 -0700

    gender option

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 angular.module('angryLibs', [])
   .controller('MainCtrl', function($scope) {
-    $scope.clearForm = function() {
-        $scope.promptsForm.$setPristine();
-    }
+    $scope.prompt = {};
+    //$scope.showStory = false;
+    // $scope.prompts = {
+    //   name:'Name',
+    //   jobTitle:'Job Title',
+    //   tediousTask:'Tedious Task',
+    //   dirtyTask:'Dirty Task',
+    //   celebrity:'Celebrity',
+    //   uselessSkill:'Useless Skill',
+    //   hugeNumber:'Huge Number',
+    //   adjective:'Adjective'
+    // };
+
+    $scope.gender = 'female';
+
+    $scope.genderize = function() {
+      if($scope.gender === 'female') {
+        $scope.nomPronoun = 'she';
+        $scope.posPronoun = 'her';
+      } else {
+        $scope.nomPronoun = 'he';
+        $scope.posPronoun = 'his';
+      }
+    };
+
+
+    $scope.reset = function() {
+      $scope.prompt = {};
+      $scope.showStory = false;
+    };
   });
